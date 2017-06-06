35a513bc5abbd320788157689908115c448f4bdd
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2017-06-05 22:46:56 -0400

    Implement simple xpath

--- Solution/ContentProxyApi/Controllers/MainController.cs
+++ Solution/ContentProxyApi/Controllers/MainController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContentContract;
 using ApiLib;
+using HtmlAgilityPack;
 
 namespace ContentProxyApi.Controllers
 {
@@ -18,8 +19,17 @@ namespace ContentProxyApi.Controllers
                 Payload = await HttpHelper.ReadStreamAsync(Request.Body)
             };
             var proxyResponse = await HttpHelper.GetAsync(new Uri(model.RequestUrl), proxyRequest);
+            var doc = new HtmlDocument();
 
-            await HttpHelper.WriteStreamAsync(proxyResponse.Payload, Response.Body);
+            doc.LoadHtml(proxyResponse.Payload);
+
+            var nodes = from path in model.Policy.XPaths
+                        select doc.DocumentNode.SelectSingleNode(path);
+            var fragments = from n in nodes
+                            select n.OuterHtml;
+            var text = string.Join(Environment.NewLine, fragments);
+
+            await HttpHelper.WriteStreamAsync(text, Response.Body);
         }
     }
 }
