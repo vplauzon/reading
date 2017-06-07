ef706f30f5c2c2661c8752012863832052d017d7
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2017-06-06 22:54:40 -0400

    Scrub html

---
+++ Solution/ContentProxyApi/ContentDocument.cs
@@ -0,0 +1,103 @@
+﻿using ContentContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace ContentProxyApi
+{
+    internal class ContentDocument
+    {
+        private readonly string _body;
+
+        public string Title { get; private set; }
+
+        public Uri NextUrl { get; private set; }
+
+        public string Html
+        {
+            get
+            {
+                return "<html>"
+                    + Environment.NewLine
+                    + "<head><title>"
+                    + Title
+                    + "</title></head>"
+                    + Environment.NewLine
+                    + "<body>"
+                    + Environment.NewLine
+                    + _body
+                    + Environment.NewLine
+                    + "</body></html>";
+            }
+        }
+
+        public static ContentDocument LoadHtml(string html, PolicyModel policy)
+        {
+            var doc = new HtmlDocument();
+
+            doc.LoadHtml(html);
+
+            var nodes = from path in policy.XPaths
+                        select doc.DocumentNode.SelectSingleNode(path);
+            var fragments = from n in nodes
+                            let scrubbed = Scrub(n)
+                            select scrubbed.OuterHtml;
+            var titleNode = doc.DocumentNode.SelectSingleNode("//title");
+            var title = titleNode == null ? string.Empty : titleNode.InnerText;
+            var body = string.Join(Environment.NewLine, fragments);
+
+            return new ContentDocument(title, null, body);
+        }
+
+        public ContentDocument Merge(ContentDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContentDocument MapImages()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ContentDocument(string title, Uri nextUrl, string body)
+        {
+            _body = body;
+            Title = title;
+            NextUrl = nextUrl;
+        }
+
+        private static HtmlNode Scrub(HtmlNode node)
+        {
+            RemoveAttribute(node, "id");
+            RemoveAttribute(node, "class");
+            RemoveElement(node, "//script");
+            RemoveElement(node, "//noscript");
+            RemoveElement(node, "//form");
+            RemoveElement(node, "//input");
+
+            return node;
+        }
+
+        private static HtmlNode RemoveAttribute(HtmlNode node, string attributeName)
+        {
+            foreach (var child in node.SelectNodes("//*[@" + attributeName + "]"))
+            {
+                child.Attributes[attributeName].Remove();
+            }
+
+            return node;
+        }
+
+        private static HtmlNode RemoveElement(HtmlNode node, string xpath)
+        {
+            foreach (var child in node.SelectNodes(xpath))
+            {
+                child.Remove();
+            }
+
+            return node;
+        }
+    }
+}
--- Solution/ContentProxyApi/Controllers/MainController.cs
+++ Solution/ContentProxyApi/Controllers/MainController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContentContract;
 using ApiLib;
-using HtmlAgilityPack;
 
 namespace ContentProxyApi.Controllers
 {
@@ -19,17 +18,10 @@ namespace ContentProxyApi.Controllers
                 Payload = await HttpHelper.ReadStreamAsync(Request.Body)
             };
             var proxyResponse = await HttpHelper.GetAsync(new Uri(model.RequestUrl), proxyRequest);
-            var doc = new HtmlDocument();
+            var document = ContentDocument.LoadHtml(proxyResponse.Payload, model.Policy);
+            var html = document.Html;
 
-            doc.LoadHtml(proxyResponse.Payload);
-
-            var nodes = from path in model.Policy.XPaths
-                        select doc.DocumentNode.SelectSingleNode(path);
-            var fragments = from n in nodes
-                            select n.OuterHtml;
-            var text = string.Join(Environment.NewLine, fragments);
-
-            await HttpHelper.WriteStreamAsync(text, Response.Body);
+            await HttpHelper.WriteStreamAsync(html, Response.Body);
         }
     }
 }
